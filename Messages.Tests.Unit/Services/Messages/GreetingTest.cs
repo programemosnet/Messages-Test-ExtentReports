17616cb59392f3591cbba8e8b0e2787534feed18
Jose Antonio Arias Rodriguez <josearias210@outlook.es>
2021-09-12 02:22:22 -0400

    test

--- Messages.Tests.Unit/Services/Messages/GreetingTest.cs
+++ Messages.Tests.Unit/Services/Messages/GreetingTest.cs
@@ -1,21 +1,52 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using Messages.Services;
 using System;
 using Xunit;
 
 namespace Messages.Tests.Unit.Services.Message
 {
-    public class GreetingTest
+    
+    public class GreetingFixture : IDisposable
     {
-        public MessageService messageService;
- 
-        public GreetingTest()
+        public ExtentReports extent;
+        public ExtentTest extentTest;
+
+        public GreetingFixture()
         {
+            var report = new ExtentHtmlReporter("index.html");
+            extent = new ExtentReports();
+            extent.AttachReporter(report);
+            extentTest = extent.CreateTest("Greeting");
+            extentTest.AssignAuthor("Programemos.net");
+        }
+
+        public void Dispose()
+        {
+            extent.Flush();
+        }
+    }
+
+    public class GreetingTest : IClassFixture<GreetingFixture>
+    {
+        public ExtentReports extent;
+        public MessageService messageService;
+        public ExtentTest extentTest;
+
+        public GreetingTest(GreetingFixture fixture)
+        {
+            extent = fixture.extent;
+            extentTest = fixture.extentTest;
             this.messageService = new MessageService();
+
         }
 
         [Fact]
-        public void Greeting_WhenNameIsNullOrEmpty_ReturnMessageGreeting()
+        public void Greeting_WhenNameIsValid_ReturnMessageGreeting()
         {
+            var node = extentTest.CreateNode("Show Greeting", "Show message greeting");
+            node.Log(Status.Info, $"Go to UR");
+
             //Arrange
             string name = "Programemos.net";
             string greeting = $"Hello {name}!!!";
@@ -24,7 +55,17 @@ namespace Messages.Tests.Unit.Services.Message
             string message = messageService.Greeting(name);
 
             //Aseerts
-            Assert.Equal(greeting,message);
+            try
+            {
+                Assert.Equal(greeting, message);
+                node.Log(Status.Pass, "Test Pass");
+            }
+            catch(Exception ex)
+            {
+                node.Fail(ex.Message);
+
+                throw ex;
+            }
         }
 
 
@@ -33,6 +74,8 @@ namespace Messages.Tests.Unit.Services.Message
         [InlineData(null)]
         public void Greeting_WhenNameIsNullOrEmpty_ReturnArgumentNullException(string name)
         {
+            var node = extentTest.CreateNode("Name empty or null", "When name is null or empty return ArgumentNullException");
+            node.Log(Status.Info, $"Go to UR");
             //Act & asserts
             var ex = Assert.Throws<ArgumentNullException>(() => this.messageService.Greeting(name));
         }
